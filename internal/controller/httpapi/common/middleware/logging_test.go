be76d0b10a057b6a61ed086f6b23b8b14ab8483e
i.razumov <1itcode1@gmail.com>
2026-07-29 20:11:44 +0200

    - os.Exit(1) в горутине внутри server.go, deferred-функции не отработают, нужен graceful shutdown через канал - - config.Load() молча глотает ошибку env.Parse, лучше возвращать ошибку - - В write.Upload нет компенсации при сбое S3 после создания записи в БД, остается зомби-аватар

---
+++ internal/controller/httpapi/common/middleware/logging_test.go
@@ -0,0 +1,37 @@
+package middleware_test
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"avatar/internal/controller/httpapi/common/middleware"
+	"avatar/internal/logger"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRequestLogger(t *testing.T) {
+	var buf bytes.Buffer
+	log := logger.New(&buf)
+
+	called := false
+	handler := middleware.RequestLogger(log)(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		called = true
+		w.WriteHeader(http.StatusTeapot)
+	}))
+
+	req := httptest.NewRequest(http.MethodGet, "/some/path", nil)
+	rec := httptest.NewRecorder()
+	handler.ServeHTTP(rec, req)
+
+	require.True(t, called)
+	assert.Equal(t, http.StatusTeapot, rec.Code)
+
+	out := buf.String()
+	assert.Contains(t, out, "request")
+	assert.Contains(t, out, "GET")
+	assert.Contains(t, out, "/some/path")
+}
